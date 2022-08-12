8a431c7c649019ded46f0d02a8bef3e5964e4bac
Travis Roth <travis@travisroth.com>
2022-08-12 13:24:00 -0500

    Updated buildVar.spy to latest tested version 2022.2

--- buildVars.py
+++ buildVars.py
@@ -36,7 +36,7 @@ It requires a Braille display to be useful."""),
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2021.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.1",
+	"addon_lastTestedNVDAVersion": "2022.2",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
@@ -71,3 +71,4 @@ baseLanguage = "en"
 # Extensions string must be of the form "markdown.extensions.extensionName"
 # e.g. "markdown.extensions.tables" to add tables.
 markdownExtensions = []
+
